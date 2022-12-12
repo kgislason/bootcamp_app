24a5f07ebcb5782ce67de6be445d9e1ff8b97c4b
Kristy Gislason <kristygislason@gmail.com>
2022-12-12 13:25:57 -0800

    Refactor teachers.js

--- teachers.js
+++ teachers.js
@@ -7,17 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortName = process.argv[2];
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${cohortName || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+const cohortName = process.argv[2] || '';
+const values = [`%${cohortName}%`];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
